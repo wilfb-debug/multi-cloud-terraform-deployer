674c712dd10504108be1ed249a0ef18a286f92db
Wilfred <wilfredballo@gmail.com>
2026-04-28 00:15:07 +0100

    Fix: update output to use v2 service URI

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "cloud_run_service_url" {
   description = "Public URL of the deployed Cloud Run service"
-  value       = google_cloud_run_service.app.status[0].url
+  value       = google_cloud_run_v2_service.app.uri
 }
 
 output "vpc_name" {
