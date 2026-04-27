d34820e1076210bcb357f58c0cf5738a9d53afba
Wilfred <wilfredballo@gmail.com>
2026-04-28 00:21:49 +0100

    Fix: add required_version and upgrade to google provider v5

--- providers.tf
+++ providers.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
